da83ac3388306e3c88214e7289afdf41b1756cc7
Bruno Fernandes <bruno.enrico99@gmail.com>
2025-03-29 03:18:13 -0300

    Pipeline completed, course in development

--- machine_learning.py
+++ machine_learning.py
@@ -1,6 +1,111 @@
-import pandas as pd
+from sklearn.model_selection import cross_validate, GroupKFold
+from sklearn.dummy import DummyClassifier
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.svm import SVC
+from typing import Any
+from sklearn.base import BaseEstimator
 import numpy as np
 
 class MachineLearning:
     def __init__(self):
-        pass
+        self.SEED = 301
+        np.random.seed(self.SEED)
+
+    @staticmethod
+    def get_dummy_classifier(**kwargs) -> DummyClassifier:
+        """
+        Returns instance of dummy classifier.
+        :param kwargs: Arguments for the dummy_classifier.
+        :return: Dummy classifier instance.
+        """
+        return DummyClassifier(**kwargs)
+
+    @staticmethod
+    def get_cross_validate(model: BaseEstimator, feature: np.ndarray, target: np.ndarray, cv: Any, **kwargs):
+        """
+        Returns cross-validated estimator.
+        :param model: Model instance.
+        :param feature: Feature data
+        :param target: Target data
+        :param cv: Number of folds.
+
+        :return: Cross-validated estimator.
+        """
+        return cross_validate(model, feature, target, cv=cv, **kwargs)
+
+    @staticmethod
+    def get_cross_validate_mean(model: dict, column: str) -> float:
+        """
+        Returns mean cross-validated estimator.
+        :param model: Model instance
+        :param column: Column name
+        :return: Mean cross-validated estimator.
+        """
+        return model[column].mean()
+
+    @staticmethod
+    def get_std_cross_validate(model: dict, column: str) -> float:
+        """
+        Returns standard deviation cross-validated estimator.
+        :param model: Model instance
+        :param column: Column to calculate
+        :return: Standard deviation of cross-validated estimator.
+        """
+        return model[column].std()
+
+
+    @staticmethod
+    def get_decision_tree_classifier(**kwargs) -> DecisionTreeClassifier:
+        """
+        Returns instance of decision tree classifier.
+        :param kwargs: Arguments for the decision_tree_classifier.
+        :return: Instance of decision tree classifier.
+        """
+        return DecisionTreeClassifier(**kwargs)
+
+
+    def print_results(self, results, column: str) -> None:
+        """
+        Prints info of the results of a model
+        :param results: Results of the model.
+        :param column: column name.
+        """
+        mean = self.get_cross_validate_mean(results, column=column)
+        std = self.get_std_cross_validate(results, column=column)
+        print("Mean cross-validated estimator:", mean)
+        print("Standard deviation cross-validated estimator:", ((mean - 2 * std) * 100), (mean + 2 * std) * 100)
+
+    @staticmethod
+    def get_groups_k_fold(**kwargs) -> GroupKFold:
+        """
+        Returns instance of groups k-fold.
+        :return: Groups k-fold instance.
+        """
+        return GroupKFold(**kwargs)
+
+    @staticmethod
+    def get_svc(**kwargs) -> SVC:
+        """
+        Gets instance of SVC.
+        :return: Instance of SVC
+        """
+        return SVC(**kwargs)
+
+    @staticmethod
+    def get_standard_scaler(**kwargs) -> StandardScaler:
+        """
+        Gets instance of standard scaler.
+        :return: instance of standard scaler
+        """
+        return StandardScaler(**kwargs)
+
+    @staticmethod
+    def get_pipeline(args: list) -> Pipeline:
+        """
+        Gets pipeline
+        :param args: List of transformer and classifier.
+        :return: Pipeline object with the given transformer and classifier.
+        """
+        return Pipeline(args)
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from data_processing import DataProcessing
 from machine_learning import MachineLearning
+import numpy as np
 
 class Main:
     def __init__(self):
@@ -9,12 +10,46 @@ class Main:
     def process(self):
         csv_data_original = self.data.get_csv_data("machine-learning-carros-simulacao.csv")
         data = self.data.drop_dataframe_columns(csv_data_original, "Unnamed: 0", axis=1)
-        print(data.head())
 
         sorted_df = self.data.sort_dataframe(data, "vendido", ascending=True)
         x_data, y_data = self.data.split_x_y_dataframe(sorted_df, ["preco", "idade_do_modelo", "km_por_ano"],
                                                                    "vendido")
         print(sorted_df.head())
+
+
+        dummy_classifier = self.machine.get_dummy_classifier()
+        cross_validate = self.machine.get_cross_validate(dummy_classifier, x_data, y_data,
+                                                         cv = 10, return_train_score=False)
+
+        mean = self.machine.get_cross_validate_mean(cross_validate, "test_score")
+        std = self.machine.get_std_cross_validate(cross_validate, "test_score")
+
+        decision_tree = self.machine.get_decision_tree_classifier(max_depth=2)
+        cross_validate = self.machine.get_cross_validate(decision_tree, x_data, y_data,
+                                                         cv=10, return_train_score=False)
+        mean = self.machine.get_cross_validate_mean(cross_validate, "test_score")
+        std = self.machine.get_std_cross_validate(cross_validate, "test_score")
+
+
+        result = np.random.randint(-2, 2, size=10000)
+        data["modelo"] = result
+        data["modelo"] = data["idade_do_modelo"] + abs(data["modelo"].min() + 1)
+
+        group_k_fold = self.machine.get_groups_k_fold(n_splits = 10)
+        model = self.machine.get_decision_tree_classifier(max_depth=2)
+        results = self.machine.get_cross_validate(model, x_data, y_data, cv=group_k_fold, groups = data["modelo"],
+                                                  return_train_score=False)
+        #self.machine.print_results(results, "test_score")
+
+        scaler = self.machine.get_standard_scaler()
+        svc = self.machine.get_svc()
+
+        pipeline = self.machine.get_pipeline([('transformer', scaler), ('classifier', svc)])
+
+        group_k_fold = self.machine.get_groups_k_fold(n_splits=10)
+        results = self.machine.get_cross_validate(pipeline, x_data, y_data, cv=group_k_fold, groups=data["modelo"],
+                                                  return_train_score=False)
+        self.machine.print_results(results, "test_score")
 
 
 if __name__ == '__main__':
